fac57744657cb83d29788481c503a0c02228b396
DiaLFonZo <dialfonzo@hotmail.com>
2017-11-06 19:13:50 -0500

    11-06-2017

--- Credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CREDENTIALS_H__
-#define __CREDENTIALS_H__
-
-  // Wifi parameters
-  char WLAN_PASS[]   	  = "Insert WLAN Password";
-  char WLAN_SSID[]   	  = "Insert WLAN SSID";
-  
-#endif
--- ESP8266_Temp_Monitor/Credentials.h
+++ ESP8266_Temp_Monitor/Credentials.h
@@ -2,11 +2,7 @@
 #define __CREDENTIALS_H__
 
   // Wifi parameters
-  char WLAN_PASS[]   	  = "macanantel";
-  char WLAN_SSID[]   	  = "DiaLFonZo_2.4";
-//  char AIO_SERVER      "io.adafruit.com"
-//  char AIO_SERVERPORT  1883
-//  char AIO_USERNAME[]   = "DiaLFonZo";
-//  char AIO_KEY[]   		  = "bac7b089668d401094526564222370a5";
+  char WLAN_PASS[]   	  = "Insert WLAN Password";
+  char WLAN_SSID[]   	  = "Insert WLAN SSID";
   
 #endif
