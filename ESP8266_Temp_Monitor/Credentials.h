a9218c4bdf9e967e73ce1a30cddd5b659eaaea40
DiaLFonZo <dialfonzo@hotmail.com>
2017-11-06 19:10:14 -0500

    11-06-2017

---
+++ ESP8266_Temp_Monitor/Credentials.h
@@ -0,0 +1,12 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+  // Wifi parameters
+  char WLAN_PASS[]   	  = "macanantel";
+  char WLAN_SSID[]   	  = "DiaLFonZo_2.4";
+//  char AIO_SERVER      "io.adafruit.com"
+//  char AIO_SERVERPORT  1883
+//  char AIO_USERNAME[]   = "DiaLFonZo";
+//  char AIO_KEY[]   		  = "bac7b089668d401094526564222370a5";
+  
+#endif
